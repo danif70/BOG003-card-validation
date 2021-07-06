4d7931dfd64cda483ea56668b23e31cd3f06ab36
gabriela <gabipaez572018@gmail.com>
2021-07-06 08:35:39 -0500

    programa funcional y foto eliminada

--- src/index.js
+++ src/index.js
@@ -1,13 +1,71 @@
 import validator from './validator.js';
-function eleccionCurso(opcion){
-    if(opcion == "Programación Básica"{
-        document.getElementById("comprar1").innerHTML=
-    }
-    ifelse(opcion == "Desarrollo Móvil") {
-        document.getElementById("comprar2").innerHTML=
-    } 
-    ifelse(opcion == "Diseño UX") {
-        document.getElementById("comprar3").innerHTML=
-    }
+
+const seleccion = "Tu Selección: ";
+const monto = "Monto : ";
+const errores1 = "Número de dígitos incorrecto";
+const errores2 = "El número de tarjeta no es válido";
+
+//en esta parte estan los eventos que se ejecutan cuando se clickea algunos de los botones//
+
+document.getElementById("comprar1").addEventListener("click", () => {
+    document.getElementById("homepage").setAttribute("hidden", "");
+    document.getElementById("paginapago").removeAttribute("hidden");
+    document.getElementById("opcion").innerHTML = seleccion + " Programacion Basica";
+    document.getElementById("precio").innerHTML = monto + " COP 50.000.oo";
+  }),
+  document.getElementById("comprar2").addEventListener("click", () => {
+    document.getElementById("homepage").setAttribute("hidden", "");
+    document.getElementById("paginapago").removeAttribute("hidden");
+    document.getElementById("opcion").innerHTML = seleccion + " Desarrollo Móvil";
+    document.getElementById("precio").innerHTML = monto + " COP 90.000.oo";
+  }),
+
+  document.getElementById("comprar3").addEventListener("click", () => {
+    document.getElementById("homepage").setAttribute("hidden", "");
+    document.getElementById("paginapago").removeAttribute("hidden");
+    document.getElementById("opcion").innerHTML = seleccion + "Diseño UX";
+    document.getElementById("precio").innerHTML = monto + " COP 50.000.oo";
+  }),
+  document.getElementById("devolver").addEventListener("click", () => {
+    document.getElementById("paginapago").setAttribute("hidden", "");
+    document.getElementById("homepage").removeAttribute("hidden");
+    document.getElementById("ingresardigitos").value = "";
+    document.getElementById("mensajeserrores").innerHTML = "";
+    
+  }),
+  document.getElementById("regresar").addEventListener("click", () => {
+    document.getElementById("paginaresultado").setAttribute("hidden", "");
+    document.getElementById("homepage").removeAttribute("hidden");
+    document.getElementById("mensajeserrores").innerHTML = "";
+  }),
+  //Este evento ecuta la función que verifique extensión de tarjeta
+  document.getElementById("resultado").addEventListener("click", () => {
+    const creditCardNumber = document.getElementById("ingresardigitos").value;
+    extension(creditCardNumber);
+  });
+//Esta funcio´n verifica que el # de tarjeta introducida tenga una extensio´n de 12 a 19 digitos
+function extension(creditCardNumber) {
+  if (creditCardNumber.length < 12 || creditCardNumber.length > 20) {
+    document.getElementById("mensajeserrores").innerHTML = errores1;
+
+  } else {
+    return verificacion(creditCardNumber);
+  }
 }
+//Esta función llama al objeto validator y el método is isValid
+function verificacion(creditCardNumber) {
+  let esValida = validator.isValid(creditCardNumber);
+  if (esValida === true) {
+    document.getElementById("paginapago").setAttribute("hidden", "");
+    document.getElementById("paginaresultado").removeAttribute("hidden");
+    enmascarar(creditCardNumber);
+  } else {
+    document.getElementById("mensajeserrores").innerText = errores2;
+  }
+}
+//Esta función llama al objeto validator y el método is Maskify
+function enmascarar(creditCardNumber) {
+  document.getElementById("verificado").innerHTML = validator.maskify(creditCardNumber);
+}
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,35 @@
 const validator = {
-  // ...
+  isValid: (creditCardNumber) => {
+    let convertir = creditCardNumber.split("").reverse();
+    let par = 0;
+    let impar = 0;
+    for (let i = 0; i < convertir.length; i++) {
+      if (i % 2 === 1) {
+        if (convertir[i] * 2 > 9) {
+          par += convertir[i] * 2 - 9;
+        } else {
+          par += convertir[i] * 2;
+        }
+      } else {
+        impar += parseInt(convertir[i]);
+      }
+    }
+    let sumatoria = par + impar;
+    if (sumatoria % 10 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  maskify: (creditCardNumber) =>{
+    let c = creditCardNumber.length;
+    let mascara= "";
+    for(let i = 0; i < c -4;i++){
+      mascara += "#";
+    }
+let numeroenmascarado = mascara + creditCardNumber.substring(c -4, c);
+return numeroenmascarado;
+  }
 };
 
 export default validator;
